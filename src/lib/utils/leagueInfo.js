eed8442780d41e178f6db0dc2fd5bb0c1e0c13a1
alexp8 <amp1993@gmail.com>
2024-08-25 16:04:55 -0400

    update sleeper id

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "916422844907630592"; // your league ID
+export const leagueID = "1071255073365331968"; // your league ID
 export const leagueName = "Cave Bois Dynasty Football"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
